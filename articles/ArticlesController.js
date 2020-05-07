d1328c4aaab8d283a72b168e97d4f3db355e702d
Lukita da Galera <lucasgo1099@gmail.com>
2020-05-06 23:19:36 -0300

    Finalização formulario artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,11 +4,21 @@
 
 const express = require('express');
 const router = express.Router();
+//Model de catgoria
+const Category = require ('../categories/Category');
 
 // Aqui vamos definir os rotas 
 
 router.get('/articles', (req, res) => {
     res.send("Rota de Artigos");
+});
+
+// Aqui vou criar o formulário
+router.get('/admin/articles/new', (req, res) => {
+    //Pesquisando todas as categorias
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    })
 });
 
 module.exports = router;
